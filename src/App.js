ad2005cb3171edd4c33cbc924784ab53885abca3
Noelle <noelle.traff@gmail.com>
2022-10-15 10:40:14 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Weather from './Weather.js'
 
 function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  let text = `Your coordinates are: (${lat.toFixed(3)}, ${lng.toFixed(3)})`
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude)
+        setLng(position.coords.longitude)
+      }, (error) => {
+        alert(error)
+      })
+    }
+    else {
+      alert('Your browser does not support geolocation!')
+    }
+  }, [])
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content" style={{margin: 50}}>
+      <p>
+        {text}
+      </p>
+      <Weather lat={lat} lng={lng} />
     </div>
   );
 }
